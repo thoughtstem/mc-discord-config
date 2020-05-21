6e7954a1686da86a12e557cd4289434184a82ad8
Stephen Foster <stephen@thoughtstem.com>
2020-05-21 14:02:22 -0700

    config

--- main.rkt
+++ main.rkt
@@ -1,7 +1,9 @@
 #lang racket/base
 
 (provide mc-core-staff-role-id
+	 mc-badge-checker-role-id
 	 mc-server-id)
 
 (define mc-core-staff-role-id "708421344936722488")
+(define mc-badge-checker-role-id "713130957502152804")
 (define mc-server-id "708421344936722482")

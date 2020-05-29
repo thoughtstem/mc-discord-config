253f7ba79aa7c7dd93544e9219877b99055030cf
Stephen R. Foster <stephen@thoughtstem.com>
2020-05-29 14:15:08 -0700

    Update main.rkt

--- main.rkt
+++ main.rkt
@@ -7,3 +7,4 @@
 (define mc-core-staff-role-id "708421344936722488")
 (define mc-badge-checker-role-id "713130957502152804")
 (define mc-server-id "708421344936722482")
+(define  mc-coach-space-station-voice-channel-id "716036392836399149")
